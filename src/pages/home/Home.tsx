d67059228804206fb564a63a01ed32e4dda2a0e7
Naram99 <farkassebi@gmail.com>
2026-06-06 23:41:15 +0200

    landing page and login page works

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,44 @@
+import { useEffect, useState } from "react";
 import styles from "./home.module.css";
+import { Link } from "react-router";
+
+const titleCarousel = ["Answer.", "Guess.", "Learn.", "Play.", "Win."];
 
 export default function Home() {
+    const [carouselCounter, setCarouselCounter] = useState(0);
+
+    useEffect(() => {
+        setInterval(() => {
+            setCarouselCounter((prev) => (prev + 1) % titleCarousel.length);
+        }, 2000);
+    }, [setCarouselCounter]);
+
+    // TODO: Carousel animation
+
     return (
         <div className={styles.homeCt}>
-            <h1 className={styles.mainTitle}>Rapidfire</h1>
+            <div className={styles.titleCt}>
+                <h1 className={styles.mainTitle}>Rapidfire</h1>
+                <div className={styles.titleCarousel}>
+                    <div className={styles.left}>
+                        {titleCarousel[carouselCounter]}
+                    </div>
+                    <div className={styles.right}>Fast.</div>
+                </div>
+            </div>
+            <div className={styles.buttonsCt}>
+                <Link to="/login" className={styles.button}>
+                    Login
+                </Link>
+                {/* <Link to="/solo" className={styles.button}>
+                    Solo play
+                </Link> */}
+            </div>
             <div className={styles.subTitle}>
                 <p>powered by</p>
-                <h2>DROVE</h2>
+                <h3>
+                    <a href="https://drove.hu">DROVE</a>
+                </h3>
             </div>
         </div>
     );
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,64 @@
 import { useNavigate } from "react-router";
+import styles from "./login.module.css";
+import { useState } from "react";
+import AuthStore from "../../zustand/authStore";
 
 export default function Login() {
     const navigate = useNavigate();
+    const user = AuthStore((state) => state.user);
 
-    return <h1>Login</h1>;
+    const [userMail, setUserMail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    if (user !== null) {
+        navigate("/dashboard", { replace: true });
+        return null;
+    }
+
+    function handleMailChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setUserMail(e.currentTarget.value);
+    }
+
+    function handlePasswordChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setPassword(e.currentTarget.value);
+    }
+
+    function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        // TODO: Login logic
+    }
+
+    return (
+        <div className={styles.loginCt}>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <div className={styles.inputCt}>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        required
+                        onChange={handleMailChange}
+                        value={userMail}
+                    />
+                </div>
+                <div className={styles.inputCt}>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        required
+                        onChange={handlePasswordChange}
+                        value={password}
+                    />
+                </div>
+                <div className={styles.errorCt}>{error && <p>{error}</p>}</div>
+                <button type="submit">Log in</button>
+            </form>
+        </div>
+    );
 }
